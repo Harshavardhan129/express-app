c79220d4775a012b78447a92b61845c1e7019306
Harshavardhan129 <160136624+Harshavardhan129@users.noreply.github.com>
2024-06-30 19:22:44 +0530

    last

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 const bcrypt = require("bcrypt");
 
-var serviceAccount = require("./key.json");
+const serviceAccount = require("./kkey.json");
 
 initializeApp({
   credential: cert(serviceAccount),
@@ -15,38 +15,41 @@ const db = getFirestore();
 
 app.set("view engine", "ejs");
 
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
 app.get("/signin", (req, res) => {
   res.render("signin");
 });
 
 app.get("/signinsubmit", (req, res) => {
   const email = req.query.email;
-  const password = req.query.password;  
-  db.collection('users')
+  const password = req.query.password;
+  
+  db.collection("users")
     .where("email", "==", email)
     .get()
-    .then((docs) => {
-      if (docs.size > 0) {
-        let userDoc;
-        docs.forEach((doc) => {
-          userDoc = doc;
-        });
-        const user = userDoc.data();
-        bcrypt.compare(password, user.password)
-          .then((match) => {
-            if (match) {
-              res.render("cod");
-            } else {
-              res.send("<h1>Login failed, incorrect login credentials</h1>");
-            }
-          })
-          .catch((error) => {
-            console.error("Error comparing passwords:", error);
-            res.send("<h1>Login failed</h1>");
-          });
-      } else {
-        res.send("<h1>Login failed, incorrect login credentials</h1>");
+    .then((snapshot) => {
+      if (snapshot.empty) {
+        return res.send("<h1>Login failed, incorrect login credentials</h1>");
       }
+      
+      const userDoc = snapshot.docs[0];
+      const user = userDoc.data();
+      
+      bcrypt.compare(password, user.password)
+        .then((match) => {
+          if (match) {
+            res.render("cod");
+          } else {
+            res.send("<h1>Login failed, incorrect login credentials</h1>");
+          }
+        })
+        .catch((error) => {
+          console.error("Error comparing passwords:", error);
+          res.send("<h1>Login failed</h1>");
+        });
     })
     .catch((error) => {
       console.error("Error checking login credentials:", error);
@@ -59,29 +62,30 @@ app.get("/signupsubmit", (req, res) => {
   const lastname = req.query.lastname;
   const email = req.query.email;
   const password = req.query.password;
-  db.collection('users')
+  
+  db.collection("users")
     .where("email", "==", email)
     .get()
-    .then((docs) => {
-      if (docs.size > 0) {
-        res.render("signfail");
-      } else {
-        bcrypt.hash(password, 10)
-          .then((hashedPassword) => {
-            return db.collection('users').add({
-              name: firstname + " " + lastname,
-              email: email,
-              password: hashedPassword,
-            });
-          })
-          .then(() => {
-            res.render("signsucc");
-          })
-          .catch((error) => {
-            console.error("Error signing up:", error);
-            res.send("Signup failed");
-          });
+    .then((snapshot) => {
+      if (!snapshot.empty) {
+        return res.render("signfail");
       }
+      
+      bcrypt.hash(password, 10)
+        .then((hashedPassword) => {
+          return db.collection("users").add({
+            name: `${firstname} ${lastname}`,
+            email: email,
+            password: hashedPassword,
+          });
+        })
+        .then(() => {
+          res.render("signsucc");
+        })
+        .catch((error) => {
+          console.error("Error signing up:", error);
+          res.send("Signup failed");
+        });
     })
     .catch((error) => {
       console.error("Error checking for existing email:", error);
